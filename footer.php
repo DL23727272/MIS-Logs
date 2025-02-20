ba24df8565cb1242d1d83ebb445adee77bec80b3
DL23727272 <113783762+DL23727272@users.noreply.github.com>
2025-02-20 11:04:23 +0800

    fixed some icons

---
+++ footer.php
@@ -0,0 +1,66 @@
+
+
+<footer style="position: relative; background: #1c1c1c; color: #ffc107;" class="mt-4 py-4">
+    <!-- Background Image -->
+    <div style="
+        /* background: url('img/nlpsc ispsc (1).png') no-repeat center center/cover; */
+        opacity: 0.2;
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+    "></div>
+
+    <div class="container position-relative" style="z-index: 2;">
+        <div class="d-lg-flex align-items-lg-center flex-lg-row mb-5 d-md-flex flex-md-column">
+            <div>
+                <img src="img/ispsc.png" alt="ISPSC Logo" width="100" height="100" class="me-3" />
+            </div>
+            <div>
+                <h1 class="ispsc-logo mb-0">REPUBLIC OF THE PHILIPPINES</h1>
+                <hr class="my-2 border-white" />
+                <h1 class="ispsc-logo mb-0">ILOCOS SUR POLYTECHNIC STATE COLLEGE</h1>
+                <h2 class="ispsc-logo mb-0">ILOCOS SUR, PHILIPPINES</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <h5>Contact Us</h5>
+                <p>
+                    Brgy. San Nicolas, Candon City<br>Ilocos Sur, Philippines
+                    <br>Email: dlvisuals@mis-main.personatab.com
+                </p>
+            </div>
+            <div class="col-md-4">
+                <h5>Follow Us</h5>
+                <p>
+                    <a href="#" class="text-decoration-none me-2 text-warning">Facebook</a>
+                    <a href="#" class="text-decoration-none me-2 text-warning">Twitter</a>
+                    <a href="#" class="text-decoration-none text-warning">Instagram</a>
+                </p>
+            </div>
+            <div class="col-md-4">
+                <h5>Quick Links</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#" class="text-decoration-none text-warning">Privacy Policy</a></li>
+                    <li><a href="#" class="text-decoration-none text-warning">Terms of Use</a></li>
+                    <li><a href="#" class="text-decoration-none text-warning">Site Map</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<!-- Bottom Copyright Section with Dynamic Year -->
+<div style="background: #4a0000; color: white; padding: 10px 0;">
+
+    <p class="text-center mb-0">
+        &copy; <span id="year"></span> Developed by Gamoso & Cabalbag. All rights reserved.
+    </p>
+</div>
+
+<!-- JavaScript to Update Year -->
+<script>
+    document.getElementById("year").textContent = new Date().getFullYear();
+</script>
--- index.php
+++ index.php
@@ -12,8 +12,16 @@
     <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/themes/bootstrap.min.css" />
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <link rel="icon" type="image/x-icon" href="img/logo.ico" />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+      integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    />
 
     <title>Validation Form</title>
+    <link href="styles.css" rel="stylesheet" />
   </head>
   <body>
 
@@ -23,7 +31,7 @@
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="addOfficeModalLabel">Add Office</h5>
+                    <h5 class="modal-title" id="addOfficeModalLabel"><i class="fa-solid fa-building"></i> Add Office</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
@@ -37,61 +45,59 @@
         </div>
     </div>
 
-      
-    <!-- Add Employee -->
-      <div class="modal fade" id="studentAddModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-           
-              <h5 class="modal-title" id="exampleModalLabel">Add Employee</h5>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+   <!-- Add Employee -->
+  <div class="modal fade" id="studentAddModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel"><i class="fa-solid fa-user-plus"></i> Add Employee</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <form id="saveInfo">
+          <div class="modal-body">
+            <div id="errorMessage" class="alert alert-warning d-none"></div>
+
+            <div class="mb-3 input-icon">
+              <label for="empID"><i class="fa-solid fa-id-badge"></i> Emp. ID</label>
+              <input type="text" name="empID" class="form-control">
             </div>
 
-                <form id="saveInfo">
-                      <div class="modal-body">
+            <div class="mb-3 input-icon">
+              <label for="name"><i class="fa-solid fa-user"></i> Name</label>
+              <input type="text" name="name" class="form-control">
+            </div>
 
-                        <div id="errorMessage" class="alert alert-warning d-none"></div>
+            <div class="mb-3 input-icon">
+              <label for="office"><i class="fa-solid fa-building"></i> Office</label>
+              <select id="office" name="office" class="form-control">
+                <option value="">Select Office</option>
+              </select>
+            </div>
 
-                        <div class="mb-3">
-                            <label for="">Emp. ID</label>
-                            <input type="text" name="empID" class="form-control">
-                        </div>
-                        <div class="mb-3">
-                            <label for="">Name</label>
-                            <input type="text" name="name" class="form-control">
-                        </div>
-                        <div class="mb-3">
-                          <label for="office">Office:</label>
-                          <select id="office" name="office" class="form-control">
-                              <option value="">Select Office</option>
-                          </select>
-                        </div>
-                        <div class="mb-3">
-                            <label for="">Position</label>
-                            <input type="text" name="position" class="form-control">
-                        </div>
-                        <div class="mb-3">
-                            <label for="">Home address</label>
-                            <input type="text" name="address" class="form-control">
-                        </div>
-                        <div class="mb-3">
-                            <label for="">Phone</label>
-                            <input type="text" name="phone" class="form-control">
-                        </div>
-                       
+            <div class="mb-3 input-icon">
+              <label for="position"><i class="fa-solid fa-briefcase"></i> Position</label>
+              <input type="text" name="position" class="form-control">
+            </div>
 
-                      </div>
-                      <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="submit" class="btn btn-primary">Save Student</button>
+            <div class="mb-3 input-icon">
+              <label for="address"><i class="fa-solid fa-home"></i> Home Address</label>
+              <input type="text" name="address" class="form-control">
+            </div>
 
-                      </div>
-                </form>
+            <div class="mb-3 input-icon">
+              <label for="phone"><i class="fa-solid fa-phone"></i> Phone</label>
+              <input type="text" name="phone" class="form-control">
+            </div>
           </div>
-        </div>
+          
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="submit" class="btn btn-primary">Save Employee</button>
+          </div>
+        </form>
       </div>
-
+    </div>
+  </div>
 
    <!-- Employee Modal -->
     <div class="modal fade" id="employeeModal" tabindex="-1" aria-labelledby="employeeModalLabel" aria-hidden="true">
@@ -105,7 +111,7 @@
             <form id="employeeForm">
               <input type="hidden" id="empID">
               <div class="mb-3">
-                <label for="empName" class="form-label">Name</label>
+                <label for="empName" class="form-label"><i class="fa-solid fa-phone"></i> Name</label>
                 <input type="text" class="form-control" id="empName" disabled>
               </div>
               <!-- <div class="mb-3">
@@ -139,82 +145,124 @@
       </div>
     </div>
 
-
-    <div class="container">
-        <div class="card">
-
-        <div class="card-header back d-flex justify-content-between align-items-center">
-            <h4>Developed by: Dran Leynard P. Gamoso</h4>
-            <div>
-                <input type="text" id="searchEmployee" class="form-control d-inline-block w-auto" placeholder="Search by name">
-                <select id="officeFilter" class="form-select d-inline-block w-auto">
-                    <option value="">All Offices</option>
-                    <?php
-                        require 'dbconnection.php';
-                        $officeQuery = "SELECT * FROM offices";
-                        $officeResult = mysqli_query($con, $officeQuery);
-                        while ($office = mysqli_fetch_assoc($officeResult)) {
-                            echo "<option value='{$office['officeID']}'>{$office['officeName']}</option>";
-                        }
-                    ?>
-                </select>
-                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addOfficeModal">
-                    Add Office
-                </button>
-                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#studentAddModal">
-                    Add Employee
-                </button>
-            </div>
-        </div>
-        </div>
-
-           <!-- Employee Table -->
-          <div class="card-body">
-              <table id="myTable" class="table table-bordered table-striped table-hover">
-                  <thead>
-                      <tr>
-                          <th>Emp. ID</th>
-                          <th>Name</th>
-                          <th>Office</th>
-                          <th>Position</th>
-                          <th>Home Address</th>
-                          <th>Phone</th>
-                          <th>Action</th>
-                      </tr>
-                  </thead>
-                  <tbody id="employeeTableBody">
-                      <?php
-                      require 'dbconnection.php';
-
-                      $query = "SELECT employees.*, offices.officeName 
-                                FROM employees 
-                                LEFT JOIN offices ON employees.officeID = offices.officeID";
-                      $query_run = mysqli_query($con, $query);
-
-                      if (mysqli_num_rows($query_run) > 0) {
-                          foreach ($query_run as $employees) {
-                              ?>
-                              <tr data-office-id="<?= $employees['officeID'] ?>">
-                                  <td><?= $employees['empID'] ?></td>
-                                  <td class="employee-name"><?= $employees['name'] ?></td>
-                                  <td><?= $employees['officeName'] ?></td>
-                                  <td><?= $employees['position'] ?></td>
-                                  <td><?= $employees['address'] ?></td>
-                                  <td><?= $employees['phone'] ?></td>
-                                  <td>
-                                      <button type="button" value="<?= $employees['empID'] ?>" class="viewEmployeebtn btn btn-secondary">View</button>
-                                  </td>
-                              </tr>
-                              <?php
-                          }
-                      }
-                      ?>
-                  </tbody>
-              </table>
+    <header class="header">
+      <div class="container">
+       <div
+        class="d-flex flex-column align-items-center justify-content-center text-center"
+        >
+          <div>
+            <img
+              src="img/ispsc.png"
+              alt="ISPSC Logo"
+              width="100"
+              height="100"
+              class="me-3"
+            />
+            <img
+              class="bagong-pilipinas"
+              src="img/bagong-pilipinas.png"
+              alt="ISPSC Logo"
+              width="120"
+              height="120"
+              class="me-3"
+            />
+          </div>
+          <div>
+            <h1 class="ispsc-logo mb-0">REPUBLIC OF THE PHILIPPINES</h1>
+            <hr class="my-2 border-white" />
+            <h1 class="ispsc-logo mb-0">
+              ILOCOS SUR POLYTECHNIC STATE COLLEGE
+            </h1>
+            <h2 class="ispsc-logo mb-0">ILOCOS SUR, PHILIPPINES</h2>
           </div>
         </div>
+      </div>
+    </header>
+
+    <div class="container">
+      <div class="card">
+          <div class="card-header back d-flex justify-content-between align-items-center">
+          
+              <div>
+                  <h4>Developed by: Dran Leynard P. Gamoso</h4>
+                  <a href="https://dlportfolio.personatab.com/" target="_blank" class="link-danger link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover fw-bold text-dark">
+                      <i class="fa-solid fa-briefcase"></i> Visit DL Portfolio
+                  </a>
+              </div>
+              <div>
+                  <i class="fa-solid fa-search"></i>
+                  <input type="text" id="searchEmployee" class="form-control d-inline-block w-auto" placeholder="Search by name">
+                  <select id="officeFilter" class="form-select d-inline-block w-auto">
+                      <option value="">All Offices</option>
+                      <?php
+                          require 'dbconnection.php';
+                          $officeQuery = "SELECT * FROM offices";
+                          $officeResult = mysqli_query($con, $officeQuery);
+                          while ($office = mysqli_fetch_assoc($officeResult)) {
+                              echo "<option value='{$office['officeID']}'>{$office['officeName']}</option>";
+                          }
+                      ?>
+                  </select>
+                  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addOfficeModal">
+                      Add Office <i class="fa-solid fa-plus"></i>
+                  </button>
+                  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#studentAddModal">
+                      Add Employee <i class="fa-solid fa-user-plus"></i>
+                  </button>
+              </div>
+          </div>
+      </div>
+
+
+        <!-- Employee Table -->
+      <div class="card-body">
+          <table id="myTable" class="table table-bordered table-striped table-hover">
+              <thead>
+                  <tr>
+                      <th><i class="fa-solid fa-id-badge"></i> Emp. ID</th>
+                      <th><i class="fa-solid fa-user"></i>  Name</th>
+                      <th><i class="fa-solid fa-building"></i> Office</th>
+                      <th><i class="fa-solid fa-briefcase"></i> Plantilla - Position</th>
+                      <th><i class="fa-solid fa-home"></i> Home Address</th>
+                      <th><i class="fa-solid fa-phone"></i> Phone</th>
+                      <th>Action</th>
+                  </tr>
+              </thead>
+              <tbody id="employeeTableBody">
+                  <?php
+                  require 'dbconnection.php';
+
+                  $query = "SELECT employees.*, offices.officeName 
+                            FROM employees 
+                            LEFT JOIN offices ON employees.officeID = offices.officeID";
+                  $query_run = mysqli_query($con, $query);
+
+                  if (mysqli_num_rows($query_run) > 0) {
+                      foreach ($query_run as $employees) {
+                          ?>
+                          <tr data-office-id="<?= $employees['officeID'] ?>">
+                              <td><?= $employees['empID'] ?></td>
+                              <td class="employee-name"><?= $employees['name'] ?></td>
+                              <td><?= $employees['officeName'] ?></td>
+                              <td><?= $employees['position'] ?></td>
+                              <td><?= $employees['address'] ?></td>
+                              <td><?= $employees['phone'] ?></td>
+                              <td>
+                                  <button type="button" value="<?= $employees['empID'] ?>" class="viewEmployeebtn btn btn-secondary">View</button>
+                              </td>
+                          </tr>
+                          <?php
+                      }
+                  }
+                  ?>
+              </tbody>
+          </table>
+      </div>
+
     </div>
-   
+    
+    <?php include "footer.php"?>
+
 
     <!-- Option 1: Bootstrap Bundle with ALertify -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
@@ -223,7 +271,20 @@
     <script src="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/alertify.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
-   <script src="script.js"></script>
+    <script src="script.js"></script>
+    <script>
+        document.addEventListener("DOMContentLoaded", function () {
+        document.querySelectorAll(".ispsc-logo").forEach(function (element) {
+            element.innerHTML = element.textContent
+              .split(" ")
+              .map(word => `<span>${word.charAt(0)}</span>${word.slice(1)}`)
+              .join(" ");
+          });
+        });
+
+
+    </script>
+
    
   </body>
                                                             
